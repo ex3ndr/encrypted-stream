3be38cc0dfca11a681ec9314ad2e65467301eb25
Steve Korshakov <steve@korshakov.com>
2020-03-14 15:19:24 -0700

    feat: Export more cryptographic primitives

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { createSigner, Endpoint, EndpointKey, Signer } from './keys';
 export { ServerEngine, ServerEngineState, PeerLoader } from './ServerEngine';
 export { ClientEngine, ClientEngineState } from './ClientEngine';
-export { newEd25519Key } from './crypto/Ed25519';
+export { newEd25519Key, sign, verify } from './crypto/Ed25519';
+export { curve25519DH, newCurve25519Key } from './crypto/Curve25519';
+export { hkdf512 } from './crypto/hkdf';
+export { openBox, sealBox } from './crypto/box';
