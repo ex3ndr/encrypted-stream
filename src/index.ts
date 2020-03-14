7b06995427c7497147e8699a92ddf735eec6d2bb
Steve Korshakov <steve@korshakov.com>
2020-03-13 23:04:47 -0700

    fix: Fix export of newEd25519Key

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { createSigner, Endpoint, EndpointKey, Signer } from './keys';
 export { ServerEngine, ServerEngineState, PeerLoader } from './ServerEngine';
 export { ClientEngine, ClientEngineState } from './ClientEngine';
+export { newEd25519Key } from './crypto/Ed25519';
